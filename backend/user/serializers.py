89e1f3fdb8d0d50d1a0286416f1b32b6fb7b0961
yashar jahanshahloo <yashar.jahanshahloo@gmail.com>
2021-07-22 14:10:16 +0430

    user patch bug fixed

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -6,14 +6,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("username", "password", "first_name", "last_name", "address", "credit", "is_staff")
-        read_only_fields = ("credit", "is_staff")
+        read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 8, "style": {"input_type": "password"}}}
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        password = validated_data.pop("password")
+        password = ""
+        if "password" in validated_data:
+            password = validated_data.pop("password")
         user = super().update(instance, validated_data)
         if password:
             user.set_password(password)
